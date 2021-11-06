cf9e86bb155219ab3113a8f069e6feae1b968c4c
Aizahzaman <79235506+Aizahzaman@users.noreply.github.com>
2021-11-06 19:47:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,55 @@ function take_pic(){
 
     console.log(ml5.version);
     var model=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/Et4Fwzep-/model.json", modelLoaded);    
+
+    function modelLoaded(){
+        console.log("model is ready to use");
+    }
+    
+    var prediction1="";
+    
+    function speak(){
+        var speak_api=window.speechSynthesis;
+    
+        speak1="The prediction is "+prediction1;
+    
+        var say_this= new SpeechSynthesisUtterance(speak1);
+    
+        speak_api.speak(say_this);
+    }
+    
+    function check(){
+        image=document.getElementById("snapshot");
+        model.classify(image,result);
+    }
+    
+    function result(error,answer){
+        if(error){
+            console.error(error);
+        } 
+        else{
+            console.log(answer);
+            document.getElementById("handgesture_name").innerHTML=answer[0].label;
+    
+            if(answer[0].label=="happy"){
+                document.getElementById("handgesture_emoji").innerHTML="&#128522";
+            }
+    
+            if(answer[0].label=="calm"){
+                document.getElementById("handgesture_emoji").innerHTML="&#128524";
+            }
+    
+            if(answer[0].label=="sad"){
+                document.getElementById("handgesture_emoji").innerHTML="&#128532";
+            }
+    
+            if(answer[0].label=="mad"){
+                document.getElementById("handgesture_emoji").innerHTML="&#128548";
+            }
+
+            }
+    
+            prediction1=answer[0].label;
+    
+            speak();
+        }
